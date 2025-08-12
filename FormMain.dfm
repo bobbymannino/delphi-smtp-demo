37bb1a5c524fa11366eb054d99079677b57c8afc
Bob Mannino <bobby.mannino@securein.com>
2025-08-12 09:38:19 +0100

    add send email button

--- FormMain.dfm
+++ FormMain.dfm
@@ -1,14 +1,44 @@
-object Form1: TForm1
+object frmMain: TfrmMain
   Left = 0
   Top = 0
-  Caption = 'Form1'
-  ClientHeight = 441
-  ClientWidth = 624
+  BorderStyle = bsSingle
+  Caption = 'SMTP Demo'
+  ClientHeight = 290
+  ClientWidth = 414
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
+  Position = poScreenCenter
   TextHeight = 15
+  object btnSend: TButton
+    AlignWithMargins = True
+    Left = 3
+    Top = 3
+    Width = 408
+    Height = 25
+    Align = alTop
+    Caption = 'Send'
+    TabOrder = 0
+    OnClick = btnSendClick
+    ExplicitLeft = 4
+    ExplicitTop = 4
+    ExplicitWidth = 706
+  end
+  object btnClose: TButton
+    AlignWithMargins = True
+    Left = 3
+    Top = 262
+    Width = 408
+    Height = 25
+    Align = alBottom
+    Caption = 'Close'
+    TabOrder = 1
+    OnClick = btnCloseClick
+    ExplicitLeft = 6
+    ExplicitTop = 11
+    ExplicitWidth = 623
+  end
 end
--- FormMain.pas
+++ FormMain.pas
@@ -3,11 +3,16 @@
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls;
 
 type
-  TForm1 = class(TForm)
+  TfrmMain = class(TForm)
+    btnSend: TButton;
+    btnClose: TButton;
+    procedure btnCloseClick(Sender: TObject);
+    procedure btnSendClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -15,10 +20,69 @@ type
   end;
 
 var
-  Form1: TForm1;
+  frmMain: TfrmMain;
 
 implementation
 
+uses UtilSMTP;
+
 {$R *.dfm}
+
+procedure TfrmMain.btnCloseClick(Sender: TObject);
+begin
+  Application.Terminate;
+end;
+
+procedure TfrmMain.btnSendClick(Sender: TObject);
+var
+  FBob: TContact;
+  FBobFake: TContact;
+  FJamie: TContact;
+  FBody: string;
+  FEmail: TEmail;
+begin
+  FBob.Name := 'Bob';
+  FBob.Email := 'manninobobby@icloud.com';
+
+  FBody := '''<div
+  style="
+    max-width: 640px;
+    background-color: #efefef;
+    padding: 32px;
+    margin-left: auto;
+    margin-right: auto;
+  "
+>
+  <h1>Hello there</h1>
+  <br />
+  <p>here i am with some<b>bold</b> and <i>italic</i> Text.</p>
+</div>
+
+<img
+  src="https://www.ashgrovecycles.co.uk/wp-content/uploads/2022/09/Fuji-E-Nevada-2.1.jpg"
+  alt="image here"
+  style="width: 100%; max-height: 400px; object-fit: cover"
+/>
+
+<div
+  style="
+    max-width: 640px;
+    background-color: #efefef;
+    padding: 32px;
+    margin-left: auto;
+    margin-right: auto;
+  "
+>
+  <a href="https://bobman.dev#about">About Me</a>
+</div>
+''';
+
+    FEmail := TEmail.Create;
+    FEmail.Subject := 'Look at my bike';
+    FEmail.Body := FBody;
+    FEmail.Recipients := [FBob];
+    FEmail.From := FBob;
+    FEmail.Send;
+end;
 
 end.
